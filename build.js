695d2af71465467fe795d00474cad90ebad247f0
Etienne Brodu <etnbrdcom@gmail.com>
2014-03-26 22:30:44 +0100

    template + build

--- build.js
+++ build.js
@@ -2,7 +2,7 @@ var fs = require('fs');
 var yaml = require('js-yaml');
 var color = require('onecolor');
 
-var te = function(tpl, data) {
+var templateEngine = function(tpl, data) {
     var re = /<<([^>]+)?>>/g;
 
     while(match = re.exec(tpl)) {
@@ -12,33 +12,54 @@ var te = function(tpl, data) {
     return tpl;
 }
 
-
-
-var context = yaml.safeLoad(fs.readFileSync('Ink.yml', 'utf8'));
-
-for (var c in context.colors) {
-	context.colors[c] = color(context.colors[c]);
-};
-
-// console.log(context);
-
-
 function write(path, name) {
 
 	var file = fs.readFileSync(path + '/' + name);
+	var content = templateEngine('' + file, context);
 
-	fs.writeFile(path + '/src/' + name, te('' + file, context), function(err, file) {
-		console.log("done !");
-	})
+	fs.writeFileSync(path + '/src/' + name, content);
 }
 
+function less(path, src, dest) {
+	var less = require('less');
+	var file = '' + fs.readFileSync(path + '/src/' + src);
 
+	if (!dest) {
+		var re = /.less$/;
+		dest = src.replace(re.exec(src), ".css");
+	}
+
+	var parser = new(less.Parser)({
+	  paths: [path + '/src/'], // Specify search paths for @import directives
+	  filename: src // Specify a filename, for better error messages
+	});
+
+	parser.parse(file, function (e, tree) {
+
+		if (e) {
+			console.error('\x1B[31m>>\x1B[39m ' + e.message);
+	  	for (var i = e.extract.length - 1; i >= 0; i--) {
+		  	console.error('  \x1B[31m' + e.extract[i] + '\x1B[39m');
+	  	};
+		} else {
+		  var css = tree.toCSS({
+		    // Minify CSS output
+		    compress: true
+		  });
+
+		  fs.writeFileSync(path + '/src/' + dest, css);
+		}
+	});
+}
 
 function build(theme) {
 	return function _build(err, file) {
-		console.log(">> " + theme)
+
+		console.log('\x1B[1m\x1B[36m>\x1B[35m>\x1B[39m\x1B[22m ' + theme)
 
 		var build = yaml.safeLoad(fs.readFileSync(theme + '/build.yml', 'utf8'));
+
+		// TEMPLATE
 
 		if (build.template instanceof Array) {
 			for (var i = build.template.length - 1; i >= 0; i--) {
@@ -47,29 +68,29 @@ function build(theme) {
 		} else {
 			write(theme, build.template);
 		}
+
+		// LESS
+		if (build.less) {
+			if (build.less instanceof Array) {
+				for (var i = build.less.length - 1; i >= 0; i--) {
+					less(theme, build.less[i]);
+				};
+			} else {
+				less(theme, build.less);
+			}
+		}
 	}
 }
 
 
+// MAIN
 
+var context = yaml.safeLoad(fs.readFileSync('Ink.yml', 'utf8'));
 
-for (var i = context.themes.length - 1; i >= 0; i--) {
-
-	fs.readFile(context.themes[i] + '/build.yml', build(context.themes[i]));
-
-
+for (var c in context.colors) {
+	context.colors[c] = color(context.colors[c]);
 };
 
-
-
-// fs.readFile('Ink.json', function(err, file) {
-
-// 	eval("var context = " + file); // TODO change that, ugly -> use either parse, or vm
-
-// 	fs.readFile('Ink.sublime-theme', function(err, file) {
-// 		fs.writeFile('Ink/Ink.sublime-theme',te('' + file, context) ,function(err, file) {
-// 			console.log("done !");
-// 		})
-
-// 	});
-// })
+for (var i = context.themes.length - 1; i >= 0; i--) {
+	fs.readFile(context.themes[i] + '/build.yml', build(context.themes[i]));
+};
