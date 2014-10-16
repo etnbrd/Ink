6637560fd76b0252893f3aa651f005735d3f2df7
Etienne Brodu <etnbrdcom@gmail.com>
2014-10-16 16:08:41 +0200

    assets (?)

--- link.sh
+++ link.sh
@@ -2,6 +2,7 @@
 
 sudo mv /usr/share/gnome-shell/theme /usr/share/gnome-shell/theme.old
 
-sudo ln -s ~/Projects/Ink					 	/usr/share/theme/Ink
+sudo ln -s ~/Projects/Ink					 	
+/usr/share/themes/Ink
 sudo ln -s ~/Projects/Ink/gnome-shell			/usr/share/gnome-shell/theme
 sudo ln -s ~/Projects/Ink/Ink.sublime-package 	~/.config/sublime-text-3/Installed\ Packages/
